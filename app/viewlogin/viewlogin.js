4ffa36f93cf63bdf6a591ec436ba6b218c2898d1
rensoftoss <gyorgy@falcon.io>
2017-01-16 11:46:29 +0100

    redirect login

--- app/viewlogin/viewlogin.js
+++ app/viewlogin/viewlogin.js
@@ -9,8 +9,23 @@ angular.module('oauth2TestApp.viewlogin', ['ngRoute'])
   });
 }])
 
-.controller('loginController', function($scope) {
+.controller('loginController', function($scope, $timeout) {
+    $scope.counter = 3;
+
+    $scope.onTimeout = function(){
+        $scope.counter--;
+        if ($scope.counter == 0) {
+            $timeout.cancel(mytimeout);
+            $scope.login();
+        }
+        else {
+            mytimeout = $timeout($scope.onTimeout, 1000);
+        }
+    };
+    var mytimeout = $timeout($scope.onTimeout,1000);
+
     $scope.login = function() {
         window.location.href = "https://api.imgur.com/oauth2/authorize?client_id=" + "CLIENT_ID_HERE" + "&response_type=token"
-    }
+    };
+
 });
